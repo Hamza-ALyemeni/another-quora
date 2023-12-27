8f29cdae54158dac871ae31f7b9934cfac8d9834
Aloud <Aloud@tad.local>
2023-12-27 17:10:08 +0300

    starting code

--- another-quora.cpp
+++ another-quora.cpp
@@ -3,6 +3,29 @@
 
 using namespace std;
 
+struct user{
+    int id;
+    string name;
+    string password;
+    string email;
+
+    user(){
+        id = -1;
+        name = "";
+        password = "";
+        email = "";
+    }
+
+    
+
+
+}
+
+struct quistion{
+    int id;
+    string name;
+}
+
 int main(){
     
 }
